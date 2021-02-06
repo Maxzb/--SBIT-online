1f76c62748617c4c35e532fe25738083f42d395b
MaxZb <47795883+Maxzb@users.noreply.github.com>
2021-02-06 16:43:16 +0300

    05

--- js/script.es5.js
+++ js/script.es5.js
@@ -4042,6 +4042,31 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
     return C.$ === S && (C.$ = Qt), e && C.jQuery === S && (C.jQuery = Yt), S;
   }, "undefined" == typeof e && (C.jQuery = C.$ = S), S;
 });
+/*!
+ * Simple jQuery Equal Heights
+ *
+ * Copyright (c) 2013 Matt Banks
+ * Dual licensed under the MIT and GPL licenses.
+ * Uses the same license as jQuery, see:
+ * http://docs.jquery.com/License
+ *
+ * @version 1.5.1
+ */
+
+!function (a) {
+  a.fn.equalHeights = function () {
+    var b = 0,
+        c = a(this);
+    return c.each(function () {
+      var c = a(this).innerHeight();
+      c > b && (b = c);
+    }), c.css("height", b);
+  }, a("[data-equal]").each(function () {
+    var b = a(this),
+        c = b.data("equal");
+    b.find(c).equalHeights();
+  });
+}(jQuery);
 !function (i) {
   "use strict";
 
@@ -4863,6 +4888,11 @@ window.onresize = function (event) {
     $('nav').insertBefore($('.skew'));
   }
 };
+/* Одинаковая высота для листа в секциях  */
+
+
+$('.section__list').css('height', '').equalHeights();
+/* $('.section__title').css('height', '').equalHeights(); */
 
 $('.hamburger').on('click', function () {
   $('.hamburger').toggleClass('is-active');
